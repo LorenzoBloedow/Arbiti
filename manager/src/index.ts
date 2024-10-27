85892a297f9fb20d0e8b4d2e3ebb9c84960144a4
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2024-10-27 12:40:44 -0400

    Init repo

---
+++ manager/src/index.ts
@@ -0,0 +1,3 @@
+// Create setup CLI function that logs in user, detects the environment and downloads the appropriate packages
+// (for example, node vs nextjs, svelte, etc), creates worker files, adds env variables and secrets, and writes setup code
+// and creates an api endpoint or server action, and other things
